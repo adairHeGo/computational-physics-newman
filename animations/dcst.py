8c186e45c93d8a7e0ec428d4365e50611677fa5e
Adair <gow3ahg@hotmail.com>
2026-01-28 15:20:21 -0600

    Exercise 9.9

---
+++ animations/dcst.py
@@ -0,0 +1,179 @@
+######################################################################
+#
+# Functions to perform fast discrete cosine and sine transforms and
+# their inverses in one and two dimensions.  These functions work
+# by wrapping the DFT function from numpy, rather than explicitly
+# performing the cosine and sine transforms themselves.
+#
+#   dct(y): Type-II discrete cosine transform (DCT) of real data y
+#   idct(a): Type-II inverse DCT of a
+#   dct2(y): 2D DCT of 2D real array y
+#   idct2(a): 2D inverse DCT real array a
+#   dst(y): Type-I discrete sine transform (DST) of real data y
+#   idst(a): Type-I inverse DST of a
+#   dst2(y): 2D DST of 2D real array y
+#   idst2(a): 2D inverse DST real array a
+#
+# The first element of the result is always zero for the sine transform.
+#
+# Written by Mark Newman <mejn@umich.edu>, June 24, 2011
+# This software is released under the Revised BSD License.  You may use,
+# share, or modify this file freely subject to the license terms below.
+#
+# Copyright 2024, Mark Newman
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+######################################################################
+
+from numpy import empty,arange,exp,real,imag,pi
+from numpy.fft import rfft,irfft
+
+
+######################################################################
+# 1D DCT Type-II
+
+def dct(y):
+    N = len(y)
+    y2 = empty(2*N,float)
+    y2[:N] = y[:]
+    y2[N:] = y[::-1]
+
+    c = rfft(y2)
+    phi = exp(-1j*pi*arange(N)/(2*N))
+    return real(phi*c[:N])
+
+
+######################################################################
+# 1D inverse DCT Type-II
+
+def idct(a):
+    N = len(a)
+    c = empty(N+1,complex)
+
+    phi = exp(1j*pi*arange(N)/(2*N))
+    c[:N] = phi*a
+    c[N] = 0.0
+    return irfft(c)[:N]
+
+
+######################################################################
+# 2D DCT
+
+def dct2(y):
+    M = y.shape[0]
+    N = y.shape[1]
+    a = empty([M,N],float)
+    b = empty([M,N],float)
+
+    for i in range(M):
+        a[i,:] = dct(y[i,:])
+    for j in range(N):
+        b[:,j] = dct(a[:,j])
+
+    return b
+
+
+######################################################################
+# 2D inverse DCT
+
+def idct2(b):
+    M = b.shape[0]
+    N = b.shape[1]
+    a = empty([M,N],float)
+    y = empty([M,N],float)
+
+    for i in range(M):
+        a[i,:] = idct(b[i,:])
+    for j in range(N):
+        y[:,j] = idct(a[:,j])
+
+    return y
+
+
+######################################################################
+# 1D DST Type-I
+
+def dst(y):
+    N = len(y)
+    y2 = empty(2*N,float)
+    y2[0] = y2[N] = 0.0
+    y2[1:N] = y[1:]
+    y2[:N:-1] = -y[1:]
+    a = -imag(rfft(y2))[:N]
+    a[0] = 0.0
+
+    return a
+
+
+######################################################################
+# 1D inverse DST Type-I
+
+def idst(a):
+    N = len(a)
+    c = empty(N+1,complex)
+    c[0] = c[N] = 0.0
+    c[1:N] = -1j*a[1:]
+    y = irfft(c)[:N]
+    y[0] = 0.0
+
+    return y
+
+
+######################################################################
+# 2D DST
+
+def dst2(y):
+    M = y.shape[0]
+    N = y.shape[1]
+    a = empty([M,N],float)
+    b = empty([M,N],float)
+
+    for i in range(M):
+        a[i,:] = dst(y[i,:])
+    for j in range(N):
+        b[:,j] = dst(a[:,j])
+
+    return b
+
+
+######################################################################
+# 2D inverse DST
+
+def idst2(b):
+    M = b.shape[0]
+    N = b.shape[1]
+    a = empty([M,N],float)
+    y = empty([M,N],float)
+
+    for i in range(M):
+        a[i,:] = idst(b[i,:])
+    for j in range(N):
+        y[:,j] = idst(a[:,j])
+
+    return y
